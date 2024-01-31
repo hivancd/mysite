7a33cc9b8e9a2ce8804e2c077762fa95f8002e66
hivancd <111187629+hivancd@users.noreply.github.com>
2024-01-31 10:39:53 -0500

    django tutorial  app

---
+++ polls/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import *
+
+# Create your views here.
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    
+    def get_queryset(self):
+        # All Questions
+        latest_questions = Question.objects.filter(pub_date__lte=timezone.now()) 
+        # Ordered by pub_date (bigger to lesser)
+        latest_questions = latest_questions.order_by("-pub_date")
+        # Only the 5 most recent
+        latest_questions = latest_questions[:5]
+        
+        return latest_questions
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
